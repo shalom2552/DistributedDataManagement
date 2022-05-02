df5d5c70dfec7b84388b4744e33d10cb4006834b
shalom2552 <shalom2552@gmail.com>
2022-05-02 14:12:53 +0300

    HW1 - init

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,18 +1,42 @@
-import mrjob
+import sys
+from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 
-def main():
-    path = "./data_HW1.pdf"
-    with open(path, 'r') as f:
-        data = f.read()
-        print(data)
+class MRValueMean(MRJob):
+    def mapper(self, _, line):
+        yield "StartedOn", line.split(',')[0].split('/')[1]
+        print(line.split(',')[0].split('/')[1])
+        yield "value", line.split(',')[1]
+        yield "lines", 1
 
+    def reducer(self, day, values):
+        yield day, sum(values)
+
+
+def main(args):
+
+    # MRValueMean().run()
+    # path = args[1]
+    path = "./data_HW1.csv"
+
+    with open(path) as f:
+        data = f.readlines()
+
+    for line in data[:10]:
+        print(line.replace("\n", ''))
+
+    MRValueMean.run()
     pass
 
 
-def ex1():
+def ex1(data):
+    # mean open value of a curtain in a day is a sum of all value of the curtain in a day divided by number of values
+    # for each month need to calculate min and max 'mean open' per day
+    # return the min and max for each month
+
     pass
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
