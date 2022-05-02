6221da6b5d6db3f3a608f567060eeffc7cc5fc2d
shalom2552 <shalom2552@gmail.com>
2022-05-02 12:27:55 +0300

    HW1 - init

---
+++ HW1/hw1.py
@@ -0,0 +1,18 @@
+import mrjob
+
+
+def main():
+    path = "./data_HW1.pdf"
+    with open(path, 'r') as f:
+        data = f.read()
+        print(data)
+
+    pass
+
+
+def ex1():
+    pass
+
+
+if __name__ == '__main__':
+    main()
